34a1f3fd691a6f5208cf4aba55b848764ff8edd5
Vlad Anikin <vanikin@provectus.com>
2017-09-28 00:32:16 +0300

    Fix migration name

--- database/migrations/2017_09_26_210433_create_treasure_table.php
+++ database/migrations/2017_09_26_210433_create_treasure_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCoinsTable extends Migration
+class CreateTreasureTable extends Migration
 {
     /**
      * Run the migrations.
