44cb47c857446f5cc35463ab070d4855779607b6
Vlad Anikin <vanikin@provectus.com>
2017-09-30 22:34:51 +0300

    Temporary test routes

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,26 @@
 |
 */
 
-Route::get('/', function () {
+
+Route::group(['prefix' => 'treasurer'], function () {
+    Route::get('test', function () {
+        $treasurer = new \App\Components\Treasurer\MasterTreasurer();
+        return response()->json($treasurer->salary()->earn(20)->getID());
+    });
 });
+
+Route::group(['prefix' => 'waster'], function () {
+    Route::get('test', function () {
+        $waster = new App\Components\Waster\MasterWaster();
+        return response()->json($waster->food()->charge(20)->getID());
+    });
+});
+
+Route::group(['prefix' => 'gringotts'], function () {
+    Route::get('test', function () {
+        $gringotts = new App\Components\Gringotts\MasterGringotts();
+        return response()->json($gringotts->credit()->open('300')->getID());
+    });
+});
+
+
