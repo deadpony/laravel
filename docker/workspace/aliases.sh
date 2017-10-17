3574fa9d7736c89bad70a87495bec49100e92d43
Vlad Anikin <vanikin@provectus.com>
2017-10-17 15:24:15 +0300

    Added helpers

--- docker/workspace/aliases.sh
+++ docker/workspace/aliases.sh
@@ -2,13 +2,13 @@
 
 # Colors used for status updates
 ESC_SEQ="\x1b["
-COL_RESET=$ESC_SEQ"39;49;00m"
-COL_RED=$ESC_SEQ"31;01m"
-COL_GREEN=$ESC_SEQ"32;01m"
-COL_YELLOW=$ESC_SEQ"33;01m"
-COL_BLUE=$ESC_SEQ"34;01m"
-COL_MAGENTA=$ESC_SEQ"35;01m"
-COL_CYAN=$ESC_SEQ"36;01m"
+COL_RESET=${ESC_SEQ}"39;49;00m"
+COL_RED=${ESC_SEQ}"31;01m"
+COL_GREEN=${ESC_SEQ}"32;01m"
+COL_YELLOW=${ESC_SEQ}"33;01m"
+COL_BLUE=${ESC_SEQ}"34;01m"
+COL_MAGENTA=${ESC_SEQ}"35;01m"
+COL_CYAN=${ESC_SEQ}"36;01m"
 
 # Detect which `ls` flavor is in use
 if ls --color > /dev/null 2>&1; then # GNU `ls`
@@ -129,8 +129,8 @@ function fs() {
 		local arg=-sh;
 	fi
 	if [[ -n "$@" ]]; then
-		du $arg -- "$@";
+		du ${arg} -- "$@";
 	else
-		du $arg .[^.]* ./*;
+		du ${arg} .[^.]* ./*;
 	fi;
 }
